391aad38abce663be3396fcdf189fb7df909e0d5
david <davidgrong778@gmail.com>
2022-05-31 09:31:59 -0500

    working survey

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -6,22 +6,42 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "horsey90"
 debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
 
 @app.route('/')
 def home_survey():
+    session['responses'] = []
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('main_survey.html', title=title, instructions=instructions)
 
 @app.route('/questions/<question_num>')
 def question_page(question_num):
+    responses = session.get('responses')
+
+    if(len(responses) != int(question_num)):
+        flash('invalid question order/number')
+        return redirect(f'/questions/{len(responses)}')
+
     cur_question = satisfaction_survey.questions[int(question_num)]
     choices = cur_question.choices
     question_text = cur_question.question
+
     return render_template('questions.html', choices=choices, question_text=question_text)
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    return "got here"
+    choice = request.form['answer']
+    responses = session['responses']
+    responses.append(choice)
+    session['responses'] = responses
+
+    if(len(responses) == len(satisfaction_survey.questions)):
+        return redirect('/finished')
+    else:
+        return redirect(f'/questions/{len(responses)}')
+
+@app.route('/finished')
+def finished():
+    return render_template('finished.html')
